58188d68392f54c84bac75967e87de360fa59764
URA Takefumi <ura.takefumi@gmail.com>
2019-07-27 17:06:53 +0900

    initial commit

---
+++ lib/uschemex.ex
@@ -0,0 +1,18 @@
+defmodule Uschemex do
+  @moduledoc """
+  Documentation for Uschemex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Uschemex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/uschemex_test.exs
@@ -0,0 +1,8 @@
+defmodule UschemexTest do
+  use ExUnit.Case
+  doctest Uschemex
+
+  test "greets the world" do
+    assert Uschemex.hello() == :world
+  end
+end
