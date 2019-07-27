cdf0b602653eb87a5f238be691303503d778cee8
URA Takefumi <ura.takefumi@gmail.com>
2019-07-28 01:35:16 +0900

    [feat] implement eval

--- lib/uschemex.ex
+++ lib/uschemex.ex
@@ -1,18 +1,80 @@
 defmodule Uschemex do
-  @moduledoc """
-  Documentation for Uschemex.
+  @doc """
+  eval(exp)
+
+  exp: S-Expression
+
+  ## Example
+  iex> Uschemex.eval([:+, 2, 3])
+  5
+  iex> Uschemex.eval([:+, [:-, 1, 2], 3])
+  2
   """
+  def eval(exp) do
+    if !list?(exp) do
+      if immediate_val?(exp) do
+        exp
+      else
+        lookup_primitive_fun(exp)
+      end
+    else
+      f = eval(car(exp))
+      args = eval_list(cdr(exp))
+      uapply(f, args)
+    end
+  end
+
+  def list?(exp) do
+    is_list(exp)
+  end
+
+  def immediate_val?(exp) do
+    is_number(exp)
+  end
+
+  defp lookup_primitive_fun(:+), do: {:prim, fn [x, y] -> x + y end}
+  defp lookup_primitive_fun(:-), do: {:prim, fn [x, y] -> x - y end}
+  defp lookup_primitive_fun(:*), do: {:prim, fn [x, y] -> x * y end}
+
+  def eval_list(exp) do
+    Enum.map(exp, fn e -> eval(e) end)
+  end
+
+  # uapply is first letter of uSchemEX
+  def uapply(f, args) do
+    apply_primitive_fun(f, args)
+  end
+
+  defp apply_primitive_fun({_, f}, args) do
+    f.(args)
+  end
 
   @doc """
-  Hello world.
+  car -- Contents of the Address part of Register
 
   ## Examples
+  iex> Uschemex.car([:a, :b, :c])
+  :a
 
-      iex> Uschemex.hello()
-      :world
-
+  iex> Uschemex.car([])
+  nil
   """
-  def hello do
-    :world
-  end
+  def car([c|_]), do: c
+  def car([]),    do: nil
+  def car(_),     do: nil
+
+
+  @doc """
+  cdr -- Contents of the Decrement part of the Register
+
+  ## Examples
+  iex> Uschemex.cdr([:a, :b, :c])
+  [:b, :c]
+
+  iex> Uschemex.cdr([])
+  []
+  """
+  def cdr([_|l]), do: l
+  def cdr([]),    do: []
+  def cdr(_),     do: []
 end
--- test/uschemex_test.exs
+++ test/uschemex_test.exs
@@ -1,8 +1,4 @@
 defmodule UschemexTest do
   use ExUnit.Case
   doctest Uschemex
-
-  test "greets the world" do
-    assert Uschemex.hello() == :world
-  end
 end
